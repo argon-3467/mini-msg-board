32e7ec46b26cee8274bad27caf6f890a9e2d7050
argon-3467 <akhildhatterwal@gmail.com>
2024-02-16 09:15:35 +0530

    Add index page
    - Add relevent styles in public/stylesheets/style.css
    - Add a new message in routes/index.js
    - Add UI in views/index.pug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date().toLocaleString('en-GB', { hour12: true }),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date().toLocaleString('en-GB', { hour12: true }),
+  },
+  {
+    text: 'You are doing great. Keep going!',
+    user: 'anonymous',
+    added: new Date().toLocaleString('en-GB', { hour12: true }),
+  },
+];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Msg Board', messages });
 });
 
 module.exports = router;
